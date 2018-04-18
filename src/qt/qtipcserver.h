f72d6afed6b7798beab14251364a466e3a21c98f
qollqrex <qollqrex@gmail.com>
2018-04-18 12:20:37 -0500

    Prueba A, Inticoin Project

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define INTICOIN-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "INTICOINURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
